27cd44e3569329ec48ec81a0228e1a266096e99a
MrDIY123 <187337219+MrDIY123@users.noreply.github.com>
2025-01-18 21:41:31 +0000

    add more code
    added more js code to play with stuff

--- jsPlayground.js
+++ jsPlayground.js
@@ -1,14 +1,29 @@
-function ageCheck (age) {
-    if (age > 18) {
-        return true; 
-    } else {
-        return confirm("stay home");
-    }
+function ageCheck(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    return confirm("stay home");
+  }
 }
-let age = prompt("How Old Are You","");
-age = (+age);
+let age = prompt("How Old Are You", "");
+age = +age;
 if (ageCheck(age)) {
-    alert("access granted");
+  alert("access granted");
 } else {
-    alert("access denied");
+  alert("access denied");
+}
+
+function priceCheck(price) {
+  if (price < 100) {
+    return true;
+  } else {
+    alert("too expensive you pesant!");
+    return false;
+  }
+}
+
+const priceResult = prompt("How spenny is your item?", "");
+
+if (priceCheck(priceResult)) {
+  prompt("How many items are you tryig to buy?", "");
 }
